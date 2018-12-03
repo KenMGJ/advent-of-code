8f233950f5325b70d1f290005464747d444ce86f
Ken Giroux <kgiroux@synacor.com>
2018-12-03 10:57:59 -0500

    Added choosing lines to print for convert

--- scripts/c64-convert.pl
+++ scripts/c64-convert.pl
@@ -3,15 +3,24 @@
 use strict;
 use warnings;
 
+my $file_name  = $ARGV[0];
+my $start_line = $ARGV[1] || 0;
+my $end_line   = $ARGV[2] || 1_000_000;
+
 my $i = 10;
 
-print "$i open 8,8,8,\"\@0:input,w,s\"\n";
+print "$i open 8,8,8,\"\@0:input,w,s\"\n" if $start_line <= $i;
 $i += 10;
 
-while (<>) {
+open (my $file, '<', $file_name) or die "Cannot open file: $!";
+
+while (<$file>) {
     chomp;
-    print "$i print#8,\"$_\"\n";
+
+    print "$i print#8,\"$_\"\n" if $start_line <= $i && $i <= $end_line;
     $i += 10;
 }
 
-print "$i close 8\n";
+print "$i close 8\n" if $i <= $end_line;
+
+close($file) || warn "close failed: $!";
