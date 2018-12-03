be7f2fa1841c721dbb9f73113601b2dcd4ff81fe
Ken Giroux <kgiroux@synacor.com>
2018-12-03 12:44:33 -0500

    Converted input file

--- scripts/c64-convert.pl
+++ scripts/c64-convert.pl
@@ -3,13 +3,15 @@
 use strict;
 use warnings;
 
-my $file_name  = $ARGV[0];
-my $start_line = $ARGV[1] || 0;
-my $end_line   = $ARGV[2] || 1_000_000;
+my $file_name     = $ARGV[0];
+my $c64_file_name = $ARGV[1] || 'input';
+my $start_line    = $ARGV[2] || 0;
+my $end_line      = $ARGV[3] || 1_000_000;
 
 my $i = 10;
 
-print "$i open 8,8,8,\"\@0:input,w,s\"\n" if $start_line <= $i;
+# print "$i open 8,8,8,\"\@0:$c64_file_name,w,s\"\n" if $start_line <= $i;
+print "open 8,8,8,\"\@0:$c64_file_name,w,s\"\n" if $start_line <= $i;
 $i += 10;
 
 open (my $file, '<', $file_name) or die "Cannot open file: $!";
@@ -17,10 +19,12 @@ open (my $file, '<', $file_name) or die "Cannot open file: $!";
 while (<$file>) {
     chomp;
 
-    print "$i print#8,\"$_\"\n" if $start_line <= $i && $i <= $end_line;
+    # print "$i print#8,\"$_\"\n" if $start_line <= $i && $i <= $end_line;
+    print "print#8,\"$_\"\n" if $start_line <= $i && $i <= $end_line;
     $i += 10;
 }
 
-print "$i close 8\n" if $i <= $end_line;
+# print "$i close 8\n" if $i <= $end_line;
+print "close 8\n" if $i <= $end_line;
 
 close($file) || warn "close failed: $!";
