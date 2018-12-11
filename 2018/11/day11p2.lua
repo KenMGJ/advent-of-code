6b8bf865b914a9cce97240c4fc339e59a238c2ed
Ken Giroux <kgiroux@synacor.com>
2018-12-11 13:56:32 -0500

    Solve part 2 with memoization

--- 2018/11/day11p2.lua
+++ 2018/11/day11p2.lua
@@ -1,14 +1,77 @@
-function get_index( x, y )
-    return 300 * ( x - 1 ) + y;
+
+local grid_serial_number = 7165
+
+function calculate_power_level( x, y )
+    local rack_id = x + 10
+    local power_level = rack_id * y
+    power_level = power_level + grid_serial_number
+    power_level = power_level * rack_id
+    power_level = tostring( power_level )
+    power_level = string.sub( power_level, -3, -3 )
+    return tonumber( power_level ) - 5
 end
 
-print(get_index( 1, 1 ))
-print(get_index( 1, 100 ))
-print(get_index( 1, 200 ))
-print(get_index( 1, 300 ))
-print(get_index( 2, 1 ))
-print(get_index( 2, 100 ))
-print(get_index( 2, 200 ))
-print(get_index( 2, 300 ))
-print(get_index( 3, 1 ))
-print(get_index( 300, 300 ))
+local grid = {}
+for x = 1, 300 do
+    grid[ x ] = {}
+    for y = 1, 300 do
+        grid[x][y] = calculate_power_level( x, y )
+    end
+end
+
+local results = {}
+setmetatable( results, { __mode = "v" } ) -- make values weak
+function get_sum_power( x, y, size )
+    local key = x .. "-" .. y .. "-" .. size
+    if results[ key ] then return results[ key ]
+    else
+        if size == 1 then
+            local result = grid[x][y]
+            results[ key ] = result
+            return result
+        else
+            local result = get_sum_power( x, y, size - 1 ) + rest( x, y, size )
+            results[ key ] = result
+            return result
+        end
+    end
+end
+
+function rest( x, y, size )
+    local sum = 0
+    for i = x, x + size - 1
+    do
+        -- print( i, x, y, size )
+        sum = sum + grid[i][y + size - 1]
+    end
+    for i = y, y + size - 2
+    do
+        sum = sum + grid[x + size - 1][i]
+    end
+    return sum
+end
+
+local max_size
+local max_x
+local max_y
+local max_power
+
+for size = 1, 300
+do
+    for i = 1, 300 - size + 1
+    do
+        for j = 1, 300 - size + 1
+        do
+            local power = get_sum_power( i, j, size )
+            if max_power == nil or max_power < power
+            then
+                max_size = size
+                max_x = i
+                max_y = j
+                max_power = power
+            end
+        end
+    end
+end
+
+print( max_x .. "," .. max_y .. "," .. max_size )
